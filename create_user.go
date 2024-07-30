9f9d992e574ee1b3315e301d2071009618279c34
George K. Thiruvathukal <gthiruvathukal@luc.edu>
2024-07-29 22:32:45 -0500

    add support for reading from YAML

--- create_user.go
+++ create_user.go
@@ -4,9 +4,13 @@ package main
 import (
     "flag"
     "fmt"
+    "io/ioutil"
+    "log"
+    "strings"
+
+    "gopkg.in/yaml.v2"
     "gorm.io/driver/sqlite"
     "gorm.io/gorm"
-    "strings"
 )
 
 type User struct {
@@ -22,17 +26,43 @@ type Tag struct {
     Name string `gorm:"uniqueIndex"`
 }
 
+type UserData struct {
+    Name  string   `yaml:"name"`
+    Email string   `yaml:"email"`
+    Note  string   `yaml:"note"`
+    Tags  []string `yaml:"tags"`
+}
+
 func main() {
     name := flag.String("name", "", "User's name")
     email := flag.String("email", "", "User's email")
     note := flag.String("note", "", "User's note")
     tags := flag.String("tags", "", "Comma-separated list of tags")
-
+    fromYaml := flag.String("from-yaml", "", "Path to YAML file with user data")
     flag.Parse()
 
-    if *name == "" || *email == "" {
-        fmt.Println("Name and email are required")
-        return
+    var userData UserData
+
+    if *fromYaml != "" {
+        data, err := ioutil.ReadFile(*fromYaml)
+        if err != nil {
+            log.Fatalf("failed to read YAML file: %v", err)
+        }
+        err = yaml.Unmarshal(data, &userData)
+        if err != nil {
+            log.Fatalf("failed to unmarshal YAML: %v", err)
+        }
+    } else {
+        userData = UserData{
+            Name:  *name,
+            Email: *email,
+            Note:  *note,
+            Tags:  strings.Split(*tags, ","),
+        }
+    }
+
+    if userData.Name == "" || userData.Email == "" {
+        log.Fatalln("Name and email are required")
     }
 
     db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
@@ -43,14 +73,14 @@ func main() {
     db.AutoMigrate(&User{}, &Tag{})
 
     var tagModels []Tag
-    for _, tagName := range strings.Split(*tags, ",") {
+    for _, tagName := range userData.Tags {
         tagName = strings.TrimSpace(tagName)
         var tag Tag
         db.FirstOrCreate(&tag, Tag{Name: tagName})
         tagModels = append(tagModels, tag)
     }
 
-    user := User{Name: *name, Email: *email, Note: *note, Tags: tagModels}
+    user := User{Name: userData.Name, Email: userData.Email, Note: userData.Note, Tags: tagModels}
     db.Create(&user)
 
     fmt.Printf("Created user: %+v\n", user)
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/yourusername/go_crud_app
 go 1.16
 
 require (
+	gopkg.in/yaml.v2 v2.4.0
 	gorm.io/driver/sqlite v1.2.5
 	gorm.io/gorm v1.23.8
 )
